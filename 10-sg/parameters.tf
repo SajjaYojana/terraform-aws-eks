12ad47c9f7df09a31f1451211d9805257c3ba24c
Yojana Sajja <yojana@Yojanas-Laptop.attlocal.net>
2024-07-29 12:01:37 -0500

    eksctl

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -1,37 +1,48 @@
-resource "aws_ssm_parameter" "db_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/db_sg_id"
-  type  = "String"
-  value = module.db.sg_id
+variable "project_name" {
+  default = "expense"
 }
 
-
-
-resource "aws_ssm_parameter" "bastion_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
-  type  = "String"
-  value = module.bastion.sg_id
+variable "environment" {
+  default = "dev"
 }
 
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
-  type  = "String"
-  value = module.vpn.sg_id
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+  }
 }
 
-resource "aws_ssm_parameter" "cluster_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/cluster_sg_id"
-  type  = "String"
-  value = module.cluster.sg_id
+variable "db_sg_description" {
+  default = "SG for DB MySQL Instances"
 }
 
-resource "aws_ssm_parameter" "node_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/node_sg_id"
-  type  = "String"
-  value = module.node.sg_id
-}
-
-resource "aws_ssm_parameter" "ingress_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/ingress_sg_id"
-  type  = "String"
-  value = module.ingress.sg_id
+variable "vpn_sg_rules" {
+  default = [
+    {
+        from_port = 943
+        to_port = 943
+        protocol = "tcp" # all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp" # all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp" # all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+        from_port = 1194
+        to_port = 1194
+        protocol = "udp" # all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
