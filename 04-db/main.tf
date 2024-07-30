aa7ba8aa7e6d814d05ff1e029d2bb84c139b2e1b
Yojana Sajja <yojana@Yojanas-Laptop.attlocal.net>
2024-07-30 12:55:54 -0500

    aws-eks

--- 04-db/main.tf
+++ 04-db/main.tf
@@ -67,7 +67,7 @@ module "db" {
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 20.0.0"
+  version = "~> 20.0.0, >=3.1.0"
   zone_name = var.zone_name
   
   records = [
